31168298d5ed6df705f53d95f6336084ebd3ead6
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-11-07 05:09:21 -0600

    end of night progress

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,53 @@
 #!/usr/bin/python
 
-import math
+# import math
 
+
+
+
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
 def recipe_batches(recipe, ingredients):
-  pass 
+    batch_count = 0
+    l = []
+    for i in recipe:
+        print("key: ", i)
+        if i in ingredients:
+            needed = recipe[i]
+            have = ingredients[i]
+            batches = 0
+            if needed <= have:
+                while needed <= have:
+                    have -= needed
+                    batches += 1
+                    print("we have =>", have)
+                    print("batches =>", batches)
+            else:
+                break
+
+            l.append(batches)
+        else:
+            l.append(0)
+    print(l)
+    batch_count = min(l)
+
+    return batch_count
+
+    
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+        
+        
+
+
+
+
+
+
+
+
+  # print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,38 @@
-#!/usr/bin/python
 
-import sys
+"""
+//Write a factorial that generates moves of n
+"""
+all_plays = [['rock'], ['paper'], ['scissors']]
+last_game = []
+new_ar = []
+def recursion(ar,num):
+  if num == 0:
+    last_game.append(ar)
+    return
+  for i in all_plays:
+    recursion(ar+[i],num-1)
+  
+  
 
 def rock_paper_scissors(n):
-  pass 
+  if n <= 0:
+    return [[]]
+  recursion(new_ar,n)
+  return last_game
+  
+
+    
+
+   
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+
+
+
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_plays = int(sys.argv[1])
+#     print(rock_paper_scissors(num_plays))
+#   else:
+#     print('Usage: rps.py [num_plays]')
